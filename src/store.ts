70e19203db0906ab666a8a0b9c319201fa477c67
Kashyap Gohil <kashyapgohil476@gmail.com>
2023-09-05 02:31:39 +0530

    [eureka] updating components that accessed global state when global state updates

--- src/store.ts
+++ src/store.ts
@@ -1,1 +1,7 @@
+export interface MapOf<T> {
+  [key: string]: T;
+}
+
 export const store: Record<string, unknown> = {};
+
+export const setter: Record<string, MapOf<Function>> = {};
--- src/useState.ts
+++ src/useState.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { store } from "./store";
+import { setter, store } from "./store";
 
 export interface StateParams {
   stateName: string;
@@ -20,12 +20,25 @@ export function generateId() {
  * @returns 3 function, getter, setter and getter for global state
  */
 
+function updateSetter(
+  id: string,
+  stateParams: { stateId: string; setterFn: Function }
+) {
+  const { stateId, setterFn } = stateParams;
+
+  if (!setter[id]) setter[id] = {};
+  if (!setter[id][stateId]) {
+    Object.assign(setter[id], { [stateId]: setterFn });
+  }
+}
+
 export function useState<T>(value: T, params?: StateParams) {
   // params
   const { stateName } = params || {};
 
   // state
   const [_, setState] = React.useState<T>(value);
+  const [__, forceRender] = React.useState<boolean>(false);
 
   const stateId = React.useMemo(() => {
     const stateId = stateName || generateId();
@@ -33,10 +46,45 @@ export function useState<T>(value: T, params?: StateParams) {
     return stateId;
   }, [stateName]);
 
+  // refs
+  const globalStateIds = React.useRef<Set<string>>(new Set());
+
   // actions
-  const getGlobalState = React.useCallback(() => {
-    return store;
-  }, []);
+  const updateGlobalState = React.useCallback(
+    (value: any) => {
+      if (!store[stateId]) {
+        Object.defineProperty(store, stateId, {
+          set(newValue) {
+            this._value = newValue;
+
+            if (setter[stateId]) {
+              for (let id of Object.keys(setter[stateId])) {
+                setter[stateId][id]?.();
+              }
+            }
+          },
+          get() {
+            return this._value;
+          },
+        });
+      } else {
+        store[stateId] = value;
+      }
+    },
+    [stateId]
+  );
+
+  const getGlobalState = React.useCallback(
+    (id: string) => {
+      updateSetter(id, {
+        stateId,
+        setterFn: () => forceRender((state) => !state),
+      });
+      globalStateIds.current.add(id);
+      return store[id];
+    },
+    [stateId]
+  );
 
   const getState = React.useCallback(() => {
     return store[stateId];
@@ -47,7 +95,7 @@ export function useState<T>(value: T, params?: StateParams) {
       if (value instanceof Function) {
         setState((state) => {
           const finalState = value(state);
-          store[stateId] = finalState;
+          updateGlobalState(finalState);
           return finalState;
         });
       } else {
@@ -55,8 +103,17 @@ export function useState<T>(value: T, params?: StateParams) {
         setState(value);
       }
     },
-    [stateId]
+    [stateId, updateGlobalState]
   );
+
+  // effects
+  React.useEffect(() => {
+    return () => {
+      for (let id of globalStateIds.current) {
+        if (setter[id][stateId]) delete setter[id][stateId];
+      }
+    };
+  }, [stateId]);
 
   return [getState, updateState, getGlobalState];
 }
